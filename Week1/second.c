787c9ee8c8307787af518d921306b73f76d12964
Hekmat Hamidi <theonehamidi@gmail.com>
2019-09-06 10:25:55 -0400

    finished take home assignment

---
+++ Week1/second.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+	printf("This\n");
+	printf("is\n");
+	printf("my\n");
+	printf("second\n");
+	printf("C\n");
+	printf("program.\n");
+	return 0;
+}
+
---
+++ Week1/third.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void){
+	printf("XOX\n");
+	printf("XXO\n");
+	printf("OOX\n");
+	return 0;
+}
+
