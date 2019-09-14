ffc246dee8b0290a4fa4294a31e5482a0c7b5524
Hekmat Hamidi <theonehamidi@gmail.com>
2019-09-14 00:22:24 -0400

    changed code

--- practice/register.c
+++ practice/register.c
@@ -8,45 +8,34 @@
 //==============================================
 
 #include <stdio.h>
-#include <math.h>
 #define _CRT_SECURE_NO_WARNINGS
-
-// Paste your in-lab code below and upgrade it to the at-home specifications
 
 
 int main(void)
 {
 	// Initialize variables
+	const float l = 1.0, q = 0.25, d = 0.10, n = 0.05, p = 0.01;
 	float amount;
-	int loonies = 0;
-	int quarters = 0;
-	float temp_amount;
-	float l_owing;
-	float q_owing;
-	float q_remainder;
+	int loonies, quarters, dime, nickle;
+	float loonies_owing, quarters_owing, dime_owing, nickle_owing, pennies_owing = 0.0, pennies;
+	// Input of amount
 	printf("Please enter the amount to be paid: $");
-	scanf_s("%f", &amount);
-	temp_amount = amount;
-	while(amount > 1)
-	{
-		loonies++;
-		amount -= 1;
-	}
-
-	l_owing = fmod(temp_amount, loonies);
-	q_owing = l_owing;
-
-	while (l_owing > 0.25)
-	{
-		l_owing -= 0.25;
-		quarters++;
-	}
-
-	q_remainder = fmod(q_owing, 0.25);
-	printf("Loonies required: %d, balance owing $%.2f\n", loonies, q_owing);
-	printf("Quarters required: %d, balance owing $%.2f", quarters, q_remainder);
+	scanf("%f", &amount);
+	// Calculation
+	loonies = amount;
+	loonies_owing = amount - loonies;
+	quarters = ((amount - loonies) / q);
+	quarters_owing = loonies_owing - (quarters * q);
+	dime = quarters_owing / d;
+	dime_owing = quarters_owing - (dime * d);
+	nickle = dime_owing / n;
+	nickle_owing = dime_owing - (nickle * n);
+	pennies = nickle_owing / p;
+	// Output
+	printf("Loonies required: %d, balance owing $%.2f\n", loonies, loonies_owing);
+	printf("Quarters required: %d, balance owing $%.2f\n", quarters, quarters_owing);
+	printf("Dimes required: %d, balance owing $%.2f\n", dime, dime_owing);
+	printf("Nickles required: %d, balance owing $%.2f\n", nickle, nickle_owing);
+	printf("Pennies required: %.f, balance owing $%.2f", pennies, pennies_owing);
 	return 0;
-
-
-
 }
