16b1cf97234fd47f290ee5c5afb62bf41d4f08e4
Hekmat Hamidi <theonehamidi@gmail.com>
2019-09-20 20:54:11 -0400

    fixed tax

--- w02/at-home/cashRegister.c
+++ w02/at-home/cashRegister.c
@@ -15,13 +15,17 @@ int main(void)
 {
 	// Initialize variables
 	const double q = 0.25, d = 0.10, n = 0.05, p = 0.01;
-	double amount;
+	double amount, tax;
 	int loonies, quarters, dime, nickle;
-	float loonies_owing, quarters_owing, dime_owing, nickle_owing, pennies_owing = 0.0, pennies;
+	double loonies_owing, quarters_owing, dime_owing, nickle_owing, pennies_owing = 0, pennies;
 	// Input of amount
 	printf("Please enter the amount to be paid: $");
 	scanf("%lf", &amount);
 	// Calculation
+	tax = amount * .13 + .005;
+	printf("GST: 1.13\n");
+	printf("Balance owing: $%.2lf\n", amount += tax);
+
 	loonies = amount;
 	loonies_owing = amount - loonies;
 	quarters = ((amount - loonies) / q);
@@ -34,11 +38,12 @@ int main(void)
 	nickle %= 5;
 	nickle_owing = dime_owing - (nickle * n);
 	pennies = nickle_owing / p;
+
 	// Output
-	printf("Loonies required: %d, balance owing $%.2f\n", loonies, loonies_owing);
-	printf("Quarters required: %d, balance owing $%.2f\n", quarters, quarters_owing);
-	printf("Dimes required: %d, balance owing $%.2f\n", dime, dime_owing);
-	printf("Nickles required: %d, balance owing $%.2f\n", nickle, nickle_owing);
-	printf("Pennies required: %.f, balance owing $%.2f\n", pennies, pennies_owing);
+	printf("Loonies required: %d, balance owing $%.2lf\n", loonies, loonies_owing);
+	printf("Quarters required: %d, balance owing $%.2lf\n", quarters, quarters_owing);
+	printf("Dimes required: %d, balance owing $%.2lf\n", dime, dime_owing);
+	printf("Nickles required: %d, balance owing $%.2lf\n", nickle, nickle_owing);
+	printf("Pennies required: %.f, balance owing $%.2lf\n", pennies, pennies_owing);
 	return 0;
 }
