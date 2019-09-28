164702b83205f4a86a8b7491e94c98186cf2f15d
Hekmat Hamidi <theonehamidi@gmail.com>
2019-09-27 22:21:28 -0400

    fixed structure

--- w03/at-home/temps.c
+++ w03/at-home/temps.c
@@ -31,6 +31,7 @@ int main()
     while((high < low || high >= 40) || low <= -40){ // if the condition is not met, decrement
       printf("Incorrect values, temperatures must be in the range -40 to 40, high must be greater than low.\n");
       i--;
+      printf("\n");
       break;
     }
 
---
+++ w03/at-lab/temps.c
@@ -0,0 +1,40 @@
+
+//==============================================
+// Name:           Full name here
+// Student Number: #########
+// Email:          hwhamidi@myseneca.ca
+// Section:        NXX
+// Workshop:       3 (in-lab)
+//==============================================
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#define NUMS 3
+
+int main()
+{
+  // initialized variables
+  int high;
+  int low;
+  // print title
+  printf("---=== IPC Temperature Analyzer ===---\n");
+  int i;
+  for(i = 1; i <= NUMS; i++) //loop through number of days
+  {
+    printf("Enter the high value for day %d: ", i);
+    scanf("%d", &high);
+    printf("\n");
+    printf("Enter the low value for day %d: ", i);
+    scanf("%d", &low);
+    printf("\n");
+    while((high < low || high >= 40) || low <= -40){ // if the condition is not met, decrement
+      printf("Incorrect values, temperatures must be in the range -40 to 40, high must be greater than low.\n");
+      i--;
+      printf("\n");
+      break;
+    }
+
+  }
+  return 0;
+}
