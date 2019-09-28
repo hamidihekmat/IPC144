785ea29f2848fafe01d6e6cdc4ac92f4150399cd
Hekmat Hamidi <theonehamidi@gmail.com>
2019-09-27 22:13:41 -0400

    fixed issues

--- w03/temps.c
+++ w03/temps.c
@@ -27,6 +27,7 @@ int main()
     printf("\n");
     printf("Enter the low value for day %d: ", i);
     scanf("%d", &low);
+    printf("\n");
     while((high < low || high >= 40) || low <= -40){ // if the condition is not met, decrement
       printf("Incorrect values, temperatures must be in the range -40 to 40, high must be greater than low.\n");
       i--;
